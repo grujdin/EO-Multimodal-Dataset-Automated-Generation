80f89ef6b7c52bd41a9241db683e953520620d93
Ion Grujdin <grujdin@gmail.com>
2025-05-08 22:13:01 +0300

    Update Launch_CEMS.py

--- Launch_CEMS.py
+++ Launch_CEMS.py
@@ -13,5 +13,5 @@ payload = {
 tok = requests.post(url, data=payload, timeout=30).json()
 print(json.dumps(tok, indent=2))
 
-acts = list_activations(limit=3, token="3NKHt6i2urmWtqOuugvr9ZjK4Fbn-AOI3Ib0xSTorPLs_y0M6yUB_sbNEjAf8kRaJH0r5cm7pm3nfUMIK0rEgQ2qN3fJTZ8kJGomTAyIs0DZFHpGj25ablQaVKUZWfBh")
+acts = list_activations(limit=3, token="<your_token>")
 print(acts[0]["name"])
